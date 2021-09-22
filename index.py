43812b12381654374a13b44f3d03bbb8f0a1304d
Hervé <herve.schmitveiler@icloud.com>
2021-09-22 13:28:50 +0800

    dwadw

--- index.py
+++ index.py
@@ -1,17 +1,1 @@
-print("Het")
-#This is just a comment I'm making for my second commit!!
-
-
-
-
-
-
-
-print("")
-print("Hello World! I have changed this!!")
-#dwadwdwawd
-#wadwdad
-
-
-
-#This is just a comment I'm making for my second commit!!
+print("Hello World")
